397bcd3e0beb770ffde01447ad14e35daccf43e6
corsen <bcchaiklin@gmail.com>
2012-02-21 20:46:43 -0500

    Simplifying easy_10

--- easy_10.rb
+++ easy_10.rb
@@ -1,11 +1,5 @@
 def isValidPhoneNumber(input)
-	matches = /^[0-9]{3}[-\.]{0,1}[0-9]{3}[-\.]{0,1}[0-9]{4}$/.match(input)
-	
-	if (matches == nil)
-		return false
-	end
-
-	return true	
+	!!matches = /^[0-9]{3}[-\.]{0,1}[0-9]{3}[-\.]{0,1}[0-9]{4}$/.match(input)
 end
 
 inputToTest = ARGV[0]
