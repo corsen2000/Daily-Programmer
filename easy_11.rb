0b19a4b2ec3193fa296730ecdbf7af99a0ea3db7
corsen <bcchaiklin@gmail.com>
2012-02-21 20:53:59 -0500

    Adding the dailyprogrammer instructions

--- easy_11.rb
+++ easy_11.rb
@@ -1,3 +1,8 @@
+=begin
+The program should take three arguments. The first will be a day, the second will be month, and the third will be year. 
+Then, your program should compute the day of the week that date will fall on.
+=end
+
 require 'date'
 
 if (ARGV.length == 3)
