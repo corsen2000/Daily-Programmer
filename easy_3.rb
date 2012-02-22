80d9975fc2c761765b6132d37ea3eb745316dfb5
Rob Hamilton <robhamilton@yahoo.com>
2012-02-21 18:46:03 -0800

    My solution for easy challenge 3

---
+++ easy_3.rb
@@ -0,0 +1,25 @@
+if (ARGV.length == 2)
+	input = ARGV[0]
+	offset = ARGV[1].to_i
+	output = Array.new(0)
+
+	input.each_byte do |c|
+		if (c >= 65 and c <= 90)
+			d = c+offset
+			if (d > 90)
+				d = d - 26
+			end
+		elsif (c >= 97 and c <= 122)
+			d = c + offset
+			if (d > 122)
+				d = d - 26
+			end
+		else
+			d = c
+		end
+
+		output.push d.chr
+	end
+
+	puts output.join
+end
