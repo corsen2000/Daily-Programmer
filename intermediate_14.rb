d7366795870c2dcadd93fffc1a46ac54dbc29f4d
Rob Hamilton <robhamilton@yahoo.com>
2012-02-23 17:27:21 -0800

    Adding some comments

--- intermediate_14.rb
+++ intermediate_14.rb
@@ -6,16 +6,20 @@ i = 1
 n = 5000
 a = Array.new(0)
 
+# Fill an array with all numbers from 1 to 4999
 begin
 	a.push(i)
 	i = i + 1	
 end while i < n
 
+# Start two counters (i and j) which will be used to remove non-prime numbers from the array (a)
 i = 1
 begin
 	j = 1
 	begin
 		numToFind = i + j + (2*i*j)
+		# If the result of the previous calculation is in the array, remove it from the
+		# array by setting that value to zero
 		if (numToFind <= a.length)
 			indexToRemove = numToFind-1 #every number's index is one less than its value
 			a[indexToRemove] = 0
@@ -27,6 +31,7 @@ begin
 	i = i + 1
 end while i < n
 
+# Iterate over the array and print out every number that is not zero.  These are the prime numbers.
 a.each { |c|
 	if (c != nil && c != 0)
 		puts ((2 * c) + 1).to_s
