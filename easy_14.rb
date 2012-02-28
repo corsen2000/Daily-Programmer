fa385a6d78a54af69e614ed4f1ee4aaccf25c4ac
corsen <bcchaiklin@gmail.com>
2012-02-27 23:34:22 -0500

    Taking this in a little

--- easy_14.rb
+++ easy_14.rb
@@ -1,5 +1,6 @@
 =begin
-Given a list of elements and a block size k, return the list of elements with every block of k elements reversed, starting from the beginning of the list.
+Given a list of elements and a block size k, return the list of elements with every block of k elements reversed, 
+starting from the beginning of the list.
 For instance, given the list 1, 2, 3, 4, 5, 6 and the block size 2, the result is 2, 1, 4, 3, 6, 5.
 =end
 
