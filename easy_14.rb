665ae529c6a162c62c773bed9916be697895a7b1
corsen <bcchaiklin@gmail.com>
2012-02-27 23:30:53 -0500

    Simplifying easy_14

--- easy_14.rb
+++ easy_14.rb
@@ -5,18 +5,10 @@ For instance, given the list 1, 2, 3, 4, 5, 6 and the block size 2, the result i
 
 input = [1,2,3,4,5,6,7,8,9,10]
 blockSize = 3
-
-input.reverse!
-
 blocks = []
 
-begin
-	block = []
-	(0..blockSize - 1).each {|item|
-		block.push(input.pop)
-	}
+input.each_slice(blockSize) do |slice|
+	blocks += slice.reverse
+end
 
-	blocks.push(block.reverse.compact)
-end while input.length > 0
-
-puts blocks.flatten
+puts blocks.join(",")
